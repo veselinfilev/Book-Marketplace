ceb368c13f5f517f78504748a8adfb499c761452
veselinfilev <veselin.filev1@abv.bg>
2023-11-08 00:26:05 +0200

    Add Catalog component and css

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,11 +5,13 @@ import Login from './Components/Login.jsx'
 import Register from './Components/Register.jsx'
 import Create from './Components/Create.jsx'
 import Details from './Components/Details.jsx'
+import Catalog from './Components/Catalog.jsx'
 
 function App() {
 
   return (
     <>
+
       <Header />
 
       {/* <HomePage/> */}
@@ -20,9 +22,12 @@ function App() {
 
       {/* <Create/> */}
 
-    <Details/>
+      <Catalog/>
+
+    {/* <Details/> */}
 
       <Footer />
+    
     </>
   )
 }

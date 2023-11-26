dd583d2a58e75faa697be7fa4bdf3604332427de
veselinfilev <veselin.filev1@abv.bg>
2023-11-26 13:48:36 +0200

    Buy functionality and dynamic sales count

--- Client/src/Components/details/Details.jsx
+++ Client/src/Components/details/Details.jsx
@@ -2,16 +2,23 @@ import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import styles from "./Details.module.css";
 import { deleteBook, getOneBook } from "../../services/bookService.js";
+import { buyBook, getBookSales } from "../../services/buyService.js";
 
 const Details = () => {
     const { bookId } = useParams();
     const [book, setBook] = useState({});
-    const navigate = useNavigate()
+    const [sales,setSales] = useState('')
+    const navigate = useNavigate();
 
     useEffect(() => {
         getOneBook(bookId)
             .then(result => setBook(result))
     }, [bookId])
+
+    useEffect(()=>{
+        getBookSales(bookId)
+        .then(result => setSales(result))
+    },[bookId])
 
     const onDelete = (bookId) => {
         deleteBook(bookId)
@@ -33,6 +40,11 @@ const Details = () => {
         navigate(`/edit/${bookId}`,{state:book});
     }
 
+    const onBuy = (bookId) =>{
+        buyBook(bookId)
+        alert('Successfuly buying')
+    }
+
     return (
         <div className={styles.bookDetails}>
             <h1>Book details</h1>
@@ -42,8 +54,9 @@ const Details = () => {
             <p>Genre: {book.genre}</p>
             <p>Price: ${book.price}</p>
             <p>Description: {book.description}</p>
+            <p>Sales: {sales}</p>
             <div className={styles.buttonContainer}>
-                <button>Buy</button>
+                <button onClick={() => onBuy(bookId)}>Buy</button>
                 <button onClick={() => onDelete(bookId)}>Delete</button>
                 <button onClick={onEdit}>Edit</button>
             </div>
